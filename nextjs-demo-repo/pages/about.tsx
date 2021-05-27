4d86c341e07f10685aa18f83ff1b3b428b6e6af3
Chuong Tran <39957959+trananhchuong@users.noreply.github.com>
2021-05-27 21:31:52 +0700

    add tailwind

---
+++ nextjs-demo-repo/pages/about.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+About.propTypes = {
+
+};
+
+function About(props) {
+    return (
+        <div className="flex items-center justify-center w-screen h-screen bg-gray-800">
+            <form method="GET">
+                <div className="relative text-gray-600 focus-within:text-gray-400">
+                    <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                        <button type="submit" className="p-1 focus:outline-none focus:shadow-outline">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" className="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                        </button>
+                    </span>
+                    <input type="search" name="q" className="py-2 text-sm text-white bg-gray-900 rounded-md pl-10 focus:outline-none focus:bg-white focus:text-gray-900" placeholder="Search..." autoComplete="off" />
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default About;
